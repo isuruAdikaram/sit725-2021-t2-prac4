df64c3184974fa699b353f559887908c6688e6c7
Isuru Adikaram <izuru775@gmail.com>
2021-08-01 22:32:16 +1000

    -Modified server.js index.html pakcage-lock.json

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ let io = require('socket.io')(http);
 
 
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
+  var user_number= Math.floor(Math.random()* user_name)
+  response.end("Hello " + user_number + "!");
 });
-
 
 //socket test
 io.on('connection', (socket) => {
