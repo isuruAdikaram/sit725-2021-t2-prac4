f95f3a9b9bf2ba5d707c5a4f63be8f6e42c5d207
Isuru Adikaram <izuru775@gmail.com>
2021-08-04 16:01:30 +1000

    -small changes

--- server.js
+++ server.js
@@ -2,45 +2,39 @@ let express = require("express");
 let app = express();
 
 //var app = require('express')();
-//let http = require('http').createServer(app);
-// let io = require('socket.io')(http);
-app.use(express.static(__dirname+'/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended:false}));
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 
 
 
 
-
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
-  var user_number= Math.floor(Math.random()* user_name)
-  response.end("Hello " + user_number + "!");
+  response.end("Hello " + user_name + "!");
 });
+
 
 //socket test
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-//   socket.on('disconnect', () => {
-//     console.log('user disconnected');
-//   });
-//   setInterval(()=>{
-//     socket.emit('number', parseInt(Math.random()*10));
-//   }, 1000);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  setInterval(()=>{
+    socket.emit('number', parseInt(Math.random()*10));
+  }, 1000);
 
-// });
-
-var port = process.env.PORT || 3000;
-
-app.listen(port,()=>{
-  console.log("Listening on port ", port);
 });
 
-s
+
+http.listen(port,()=>{
+  console.log("Listening on port ", port);
+});
 
 //this is only needed for Cloud foundry 
 require("cf-deployment-tracker-client").track();
