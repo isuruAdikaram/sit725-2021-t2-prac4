2ea46e804729393555b071bd777d4731dc5db4e1
Isuru Adikaram <izuru775@gmail.com>
2021-08-10 20:48:31 +1000

    -styles change -array or 2 objects added

--- public/js/env.js
+++ public/js/env.js
@@ -1,3 +1,18 @@
+const itemsList =[
+  {
+    title:"Cowardly-cat",
+    image:"assets/cat-1.jpg",
+    link:"stupidcats.com",
+    descripton:"wacked birds all day"
+  },
+  {
+    title:"Enlightened-cat",
+    image:"assets/cat-2.jpg",
+    link:"evenstupidercats.com",
+    descripton:"harased dogs all day"
+  }
+]
+
 const testButtonFunction=()=>{
   alert('Thank you for clicking, sign up button is not yet active!')
 }
@@ -10,8 +25,25 @@ const testButtonFunction=()=>{
 // socket.on('number', (msg) => {
 //     console.log('Random number: ' + msg);
 // })
+const addcard =(items)=>{
+  items.forEach(item => {
+    let itemToAppend = 
+    '<div class="col s4">'+
+    ' <div class="card medium">'+
+    '<div class="card-image waves-effect waves-block waves-light">'+
+    '<img class="activator" src="'+item.image+'"></div>'+
+    '<div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span>'+
+    '<p><a href="#">about</a></p>'+
+    '</div>'+
+    '<div class="card-reveal">'+
+    '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+    '<p>Here is some more information about this product that is only revealed once clicked on.</p>'+
+    ' </div></div></div></div>';
+    $('#card-section').append(itemToAppend)
+  });
+}
 
-console.log('test')
 $(document).ready(function(){
   console.log('Ready')
   
@@ -22,6 +54,7 @@ $(document).ready(function(){
   $.get('/test?user_name="Fantastic User"',(result)=>{
     console.log(result)
   })
+  addcard(itemsList);
 
 
 })
