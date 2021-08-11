13e4236ef49775a5ac19ad9f8b2925670a4c7d8c
Isuru Adikaram <izuru775@gmail.com>
2021-08-11 16:26:38 +1000

    - update card from serverside added

--- public/js/env.js
+++ public/js/env.js
@@ -3,28 +3,60 @@ const itemsList =[
     title:"Cowardly-cat",
     image:"assets/cat-1.jpg",
     link:"stupidcats.com",
-    descripton:"wacked birds all day"
+    description:"wacked birds all day"
   },
   {
     title:"Enlightened-cat",
     image:"assets/cat-2.jpg",
     link:"evenstupidercats.com",
-    descripton:"harased dogs all day"
+    description:"harased dogs all day"
   }
 ]
 
 const testButtonFunction=()=>{
   alert('Thank you for clicking, sign up button is not yet active!')
 }
+const addProjectToApp =(project)=>{
+  $.ajax({
+    url: '/api/projects',
+    data: project,
+    type: 'POST',
+    success: (result) => {
+        alert(result.message);
+        location.reload();
+    }
+})
+}
 
-// connect to the socket
+const submitForm = ()=>{
+  const formData ={}
+  formData.title = $('#title').val()
+  formData.image = $('#image').val()
+  formData.link = $('#link').val()
+  formData.description = $('#description').val()
 
-// let socket = io();
+  console.log("form data: ",formData)
+  console.log("type of form data: ", typeof formData)
+  
+  // let array =[]
+  // array.push(formData)
+  // console.log(array)
+  // addcard(array)
+  addProjectToApp(formData)
+}
 
+const getProjects = () => {
+  $.get('/api/projects',(response) => {
+      if(response.statusCode==200){
+          console.log(response)
+          addcard(response.data);
+      }
+      else {
+          console.log(response)
+      }
+  })
+}
 
-// socket.on('number', (msg) => {
-//     console.log('Random number: ' + msg);
-// })
 const addcard =(items)=>{
   items.forEach(item => {
     let itemToAppend = 
@@ -46,15 +78,21 @@ const addcard =(items)=>{
 
 $(document).ready(function(){
   console.log('Ready')
-  
+  $('#formSubmit').click(()=>{
+    submitForm();
+  })
   //bind the button
-  $('#testButton').click(testButtonFunction)
+  // $('#testButton').click(testButtonFunction)
 
   //test get call
-  $.get('/test?user_name="Fantastic User"',(result)=>{
-    console.log(result)
-  })
-  addcard(itemsList);
+  // $.get('/test?user_name="Fantastic User"',(result)=>{
+  //   console.log(result)
+  // })
+  // addcard(itemsList);
+  getProjects();
+  $('.modal').modal();
+  
+  
 
 
 })
--- server.js
+++ server.js
@@ -1,10 +1,32 @@
+require('dotenv').config()
 let express = require("express");
 let app = express();
+var cors = require("cors")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
 
 //var app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
+
+const itemsList =[
+  {
+    title:"Cowardly-cat",
+    image:"assets/cat-1.jpg",
+    link:"stupidcats.com",
+    description:"wacked birds all day"
+  },
+  {
+    title:"Enlightened-cat",
+    image:"assets/cat-2.jpg",
+    link:"evenstupidercats.com",
+    description:"harased dogs all day"
+  }
+]
 
 
 
@@ -13,10 +35,33 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/test", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
+app.get("/test",  (req, res) =>{
+  var user_name = req.query.user_name;
+  res.end("Hello " + user_name + "!");
 });
+
+app.get('/api/projects',(req,res) => {
+  // getProjects((err,result) => {
+  //     if(err) {
+  //         res.json({statusCode: 400, message: err})
+  //     }
+  //     else {
+  //         res.json({statusCode: 200, message:"Success", data: result})
+  //     }
+  // })
+  res.json({statusCode: 200, data: itemsList, message:"Success"})
+})
+
+
+app.post('/api/projects',(req,res) => {
+  console.log("New Project added", req.body)
+  let newProject = req.body;
+  itemsList.push(newProject)
+  // console.log(itemsList)
+  console.log(newProject)
+  // res.json({statusCode: 200, message:"Project Successfully added", data: newProject})
+  res.json({})
+})
 
 
 //socket test
