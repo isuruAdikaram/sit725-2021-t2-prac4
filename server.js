0a27db38075b60e70bb8cc981c381d94550ac8f0
Isuru Adikaram <izuru775@gmail.com>
2021-08-11 17:30:09 +1000

    -database connection added

--- server.js
+++ server.js
@@ -2,11 +2,30 @@ require('dotenv').config()
 let express = require("express");
 let app = express();
 var cors = require("cors")
+const {MongoClient} = require('mongodb')
+
+// Database connection
+
+const uri = "mongodb+srv://SIT725:g7rPA5zP1991@sit-725-2021-t2-prac4.1mguh.mongodb.net/SIT-725-2021-t2-Prac4?retryWrites=true&w=majority"
+const client = new MongoClient(uri,{useNewUrlParser:true})
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
+const createColllection = (collectionName) => {
+  client.connect((err,db) => {
+      projectCollection = client.db().collection(collectionName);
+      if(!err) {
+          console.log('MongoDB Connected')
+      }
+      else {
+          console.log("DB Error: ", err);
+          process.exit(1);
+      }
+  })
+}
 
 //var app = require('express')();
 let http = require('http').createServer(app);
@@ -35,32 +54,44 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/test",  (req, res) =>{
-  var user_name = req.query.user_name;
-  res.end("Hello " + user_name + "!");
-});
+// insert into database
+const insertProjects = (project,callback) => {
+  projectCollection.insert(project,callback);
+}
+
+// get projects from the database
+const getProjects = (callback) => {
+  projectCollection.find({}).toArray(callback);
+}
 
 app.get('/api/projects',(req,res) => {
-  // getProjects((err,result) => {
-  //     if(err) {
-  //         res.json({statusCode: 400, message: err})
-  //     }
-  //     else {
-  //         res.json({statusCode: 200, message:"Success", data: result})
-  //     }
-  // })
-  res.json({statusCode: 200, data: itemsList, message:"Success"})
+  getProjects((err,result) => {
+      if(err) {
+          res.json({statusCode: 400, message: err})
+      }
+      else {
+          res.json({statusCode: 200, message:"Success", data: result})
+      }
+  })
+  // res.json({statusCode: 200, data: itemsList, message:"Success"})
 })
 
 
 app.post('/api/projects',(req,res) => {
   console.log("New Project added", req.body)
   let newProject = req.body;
-  itemsList.push(newProject)
+  // itemsList.push(newProject)
   // console.log(itemsList)
-  console.log(newProject)
+  insertProjects(newProject,(err,result) => {
+    if(err) {
+        res.json({statusCode: 400, message: err})
+    }
+    else {
+        res.json({statusCode: 200, message:"Project Successfully added", data: result})
+    }
+})
   // res.json({statusCode: 200, message:"Project Successfully added", data: newProject})
-  res.json({})
+  
 })
 
 
@@ -79,6 +110,7 @@ io.on('connection', (socket) => {
 
 http.listen(port,()=>{
   console.log("Listening on port ", port);
+  createColllection("pets")
 });
 
 //this is only needed for Cloud foundry 
