a00ce014387d73ded85c80e76e5c396ffa8ba355
Isuru Adikaram <izuru775@gmail.com>
2021-08-23 17:22:46 +1000

    routes created

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 let express = require("express");
+const indexRoute =require('./routes/projects')
 let app = express();
 var cors = require("cors")
 const {MongoClient} = require('mongodb')
+
+
 
 // Database connection
 
@@ -54,45 +57,27 @@ var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-// insert into database
-const insertProjects = (project,callback) => {
-  projectCollection.insert(project,callback);
-}
-
-// get projects from the database
-const getProjects = (callback) => {
-  projectCollection.find({}).toArray(callback);
-}
-
-app.get('/api/projects',(req,res) => {
-  getProjects((err,result) => {
-      if(err) {
-          res.json({statusCode: 400, message: err})
-      }
-      else {
-          res.json({statusCode: 200, message:"Success", data: result})
-      }
-  })
-  // res.json({statusCode: 200, data: itemsList, message:"Success"})
-})
 
 
-app.post('/api/projects',(req,res) => {
-  console.log("New Project added", req.body)
-  let newProject = req.body;
-  // itemsList.push(newProject)
-  // console.log(itemsList)
-  insertProjects(newProject,(err,result) => {
-    if(err) {
-        res.json({statusCode: 400, message: err})
-    }
-    else {
-        res.json({statusCode: 200, message:"Project Successfully added", data: result})
-    }
-})
-  // res.json({statusCode: 200, message:"Project Successfully added", data: newProject})
+app.use(indexRoute)
+
+
+// app.post('/api/projects',(req,res) => {
+//   console.log("New Project added", req.body)
+//   let newProject = req.body;
+//   // itemsList.push(newProject)
+//   // console.log(itemsList)
+//   insertProjects(newProject,(err,result) => {
+//     if(err) {
+//         res.json({statusCode: 400, message: err})
+//     }
+//     else {
+//         res.json({statusCode: 200, message:"Project Successfully added", data: result})
+//     }
+// })
+//   // res.json({statusCode: 200, message:"Project Successfully added", data: newProject})
   
-})
+// })
 
 
 //socket test
